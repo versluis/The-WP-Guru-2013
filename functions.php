8de3b3ab5628c33bcb96a47cbc8e6525bd821b23
Jay Versluis <versluis2000@yahoo.com>
2018-09-11 18:37:21 -0400

    initial commit

---
+++ functions.php
@@ -0,0 +1,54 @@
+<?php /*
+
+  This file is part of a child theme called The WP Guru 2018.
+  Functions in this file will be loaded before the parent theme's functions.
+  For more information, please read https://codex.wordpress.org/Child_Themes.
+
+*/
+
+// this code loads the parent's stylesheet (leave it in place unless you know what you're doing)
+
+function theme_enqueue_styles() {
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array($parent_style));
+}
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
+/*  Add your own functions below this line.
+    ======================================== */ 
+
+/* remove JetPack upsell messages */
+/* https://mattreport.com/disable-jetpack-upsell-ads/ */
+add_filter( 'jetpack_just_in_time_msgs', '_return_false' );
+
+
+/* Word Count functions */
+
+function guru_getWordCountFromPosts () {
+	
+	$count = 0;
+	$posts = get_posts( array(
+			'numberposts' => -1,
+			'post_type' => 'any'
+	));
+	
+	foreach( $posts as $post ) {
+		$count += str_word_count( strip_tags( get_post_field( 'post_content', $post->ID )));
+	}
+	$num =  number_format_i18n( $count );
+	
+	return $count;
+}
+
+function guru_getWordCountCommentsCurrentUser() {
+	
+	$count = 0;
+	$user_id = get_current_user_id();
+	$comments = get_comments( array(
+			'user_id' => $user_id
+	));
+	foreach( $comments as $comment ) {
+		$count += str_word_count( $comment->comment_content );
+	}
+	return $count;
+}
