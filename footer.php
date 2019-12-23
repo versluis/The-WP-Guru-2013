f9269a9bdf6fd7a1d4d09c42ed2f1d0ed5a1f82e
Jay Versluis <versluis2000@yahoo.com>
2019-12-23 11:04:00 -0500

    added affiliate disclosure to footer

---
+++ footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<?php get_sidebar( 'main' ); ?>
+
+			<div class="site-info">
+				<?php do_action( 'twentythirteen_credits' ); ?>
+				<?php
+				if ( function_exists( 'the_privacy_policy_link' ) ) {
+					the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+				}
+				?>
+				Some links on this site generate an affiliated. The revenue offsets content and infrastructure cost. There are <a href="https://wpguru.co.uk/support/">many ways to support me<a>. 😍
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
