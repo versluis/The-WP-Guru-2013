638d3b39cd50161d017c17c6d32af7ad6a80cda1
Jay Versluis <versluis2000@yahoo.com>
2020-08-04 09:31:08 -0400

    code refactoring

--- functions.php
+++ functions.php
@@ -17,70 +17,14 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 /*  Add your own functions below this line.
     ======================================== */ 
 
-include 'includes/podcast-badge.php';
+// includes
+include plugin_dir_path (__FILE__) . 'includes/podcast-badge.php';
+include plugin_dir_path (__FILE__) . 'includes/wordcount.php';
+include plugin_dir_path (__FILE__) . 'includes/roles.php';
 
 /* remove JetPack upsell messages */
 /* https://mattreport.com/disable-jetpack-upsell-ads/ */
 add_filter( 'jetpack_just_in_time_msgs', '_return_false' );
-
-/* ---------------------
-/* Word Count functions 
-/* ---------------------*/
-function guru_getWordCountFromPosts () {
-	
-	$count = 0;
-	$posts = get_posts( array(
-			'numberposts' => -1,
-			'post_type' => 'any'
-	));
-	
-	foreach( $posts as $post ) {
-		$count += str_word_count( strip_tags( get_post_field( 'post_content', $post->ID )));
-	}
-	$num =  number_format_i18n( $count );
-	
-	return $count;
-}
-
-function guru_getWordCountCommentsCurrentUser() {
-	
-	$count = 0;
-	$user_id = get_current_user_id();
-	$comments = get_comments( array(
-			'user_id' => $user_id
-	));
-	foreach( $comments as $comment ) {
-		$count += str_word_count( $comment->comment_content );
-	}
-	return $count;
-}
-
-function clickable_content ($content){
-	
-	$content = make_clickable($content);
-	return $content;
-}
-add_filter ('the_content', 'clickable_content');
-
-//
-// add file uploads to Contributor role
-// @since 1.4
-// 
-function guru_add_uploads_to_contribs () {
-	global $pagenow;
-	
-	// grab contribtor role
-	$role = get_role ('contributor');
-	
-	// if this theme is activated, add the upload capability
-	if ('themes.php' == $pagenow && isset ($_GET['activated'])) {
-		$role -> add_cap ('upload_files');
-	} else {
-		// remove when deactivated
-		$role -> remove_cap ('upload_files');
-	}
-}
-add_action ('load-themes.php', 'guru_add_uploads_to_contribs');
 
 
 //
